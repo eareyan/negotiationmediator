f39c57ba3fda99d054181bbf5c1d8bf0f94bea76
eareyan <enrique3@gmail.com>
2018-12-20 13:21:33 +0900

    Base Path dir in util

--- util/util.py
+++ util/util.py
@@ -1,16 +1,20 @@
+my_base_dir = '/Users/enriqueareyan/Documents/workspace/negotiationmediator/'
+
+
 def get_scenario_path(num_issues, domain_size, num_constraints):
-    return '/Users/enriqueareyan/Documents/workspace/negotiationmediator/scenarios/num_issues_' \
+    return my_base_dir + 'scenarios/num_issues_' \
            + str(num_issues) + '/domain_size_' \
            + str(domain_size) + '/num_constraints_' \
            + str(num_constraints) + '/'
 
 
 def get_db_path():
-    return "/Users/enriqueareyan/Documents/workspace/negotiationmediator/db/"
+    return my_base_dir + 'db/'
 
 
 def get_lisp_compiler_path():
-    return "/Library/ccl/dx86cl64"
+    return '/Library/ccl/dx86cl64'
+
 
 def get_lisp_scenariogen_path():
-    return "/Users/enriqueareyan/Documents/workspace/negotiationmediator/lisp/scenariogen.lisp"
+    return my_base_dir + 'lisp/scenariogen.lisp'
